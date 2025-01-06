23fbe8c9f569c6fc99918b0a57a3174dfead58b3
Otu Udo <119851008+Otumiky@users.noreply.github.com>
2025-01-06 17:41:59 +0100

    Update variable.tf

--- project-root/modules/asg/variable.tf
+++ project-root/modules/asg/variable.tf
@@ -1,5 +1,9 @@
 variable "ami_id" {}
 variable "instance_type" {}
+variable "subnet_ids" {
+  description = "List of subnet IDs for the Auto Scaling Group"
+  type        = list(string)
+}
 variable "subnet_id" {}
 variable "desired_capacity" {}
 variable "max_size" {}
