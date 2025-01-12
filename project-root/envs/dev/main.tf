22f8a6308c15f695f11043f93afced933646c97e
Otu Udo <119851008+Otumiky@users.noreply.github.com>
2025-01-12 15:16:38 +0100

    2nd commit

--- project-root/envs/dev/main.tf
+++ project-root/envs/dev/main.tf
@@ -1,49 +1,30 @@
-provider "aws" {
-  region = "us-east-1"
-}
 module "vpc" {
-  source         = "C://Users//madus//Desktop//30 Days of Terraform//30-Day-Terraform-challenge-//Day26//project-root//modules//vpc"
-  vpc_cidr       = "10.0.0.0/16"
-  vpc_name       = "my-vpc"
-  web_subnet_1_cidr   = "10.0.1.0/24"
-  web_subnet_2_cidr = "10.0.2.0/24"
-  subnet_1_az     = "us-east-1a"
-  subnet_2_az     = "us-east-1b"
-}
-module "ec2" {
-  source        = "C://Users//madus//Desktop//30 Days of Terraform//30-Day-Terraform-challenge-//Day26//project-root//modules//ec2"
-  ami_id        = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = module.vpc.subnet_1
-
-  user_data     = base64encode(<<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y httpd
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-              echo "Hello from Server $(hostname)" > /var/www/html/index.html
-              EOF
-  )
-
-}
-module "elb" {
-  source             = "C://Users//madus//Desktop//30 Days of Terraform//30-Day-Terraform-challenge-//Day26//project-root//modules//elb"
-  subnet_ids         = [module.vpc.subnet_1, module.vpc.subnet_2]
-  vpc_id             = module.vpc.vpc_id
-  elb_security_group = module.vpc.elb_sg
+  source             = "C://Users//madus//Desktop//30 Days of Terraform//two-tier//module//vpc"
+  cidr_block         = "10.0.0.0/16"
+  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
+  availability_zones = ["us-east-1a", "us-east-1b"]
+  vpc_name           = "two-tier-vpc"
 }
 
 module "asg" {
-  source           = "C://Users//madus//Desktop//30 Days of Terraform//30-Day-Terraform-challenge-//Day26//project-root//modules//asg"
-  ami_id           = var.ami_id
-  instance_type    = var.instance_type
-  subnet_ids       = [module.vpc.subnet_1, module.vpc.subnet_2]
-  subnet_id        = module.vpc.subnet_1
-              
-
+  source          = "C://Users//madus//Desktop//30 Days of Terraform//two-tier//module//asg"
+  name_prefix     = "web-tier"
+  image_id        = "ami-0453ec754f44f9a4a"
+  instance_type   = "t2.micro"
+  security_groups = module.vpc.asg_sg_id
+  subnets         = module.vpc.public_subnets
   desired_capacity = 2
-  max_size         = 3
-  min_size         = 1
-  target_group_arn = module.elb.target_group_arn
+  min_size        = 1
+  max_size        = 3
+  target_group_arn = module.alb.target_group_arn
+  user_data       = var.user_data
+  
+}
+
+module "alb" {
+  source          = "C://Users//madus//Desktop//30 Days of Terraform//two-tier//module//elb"
+  name            = "web-alb"
+  security_groups = module.vpc.alb_sg_id
+  subnets         = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
 }
